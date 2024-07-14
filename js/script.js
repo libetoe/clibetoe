a0b77bca3a9f38009bd741c0302881659e3c0212
libetoe <33245313+libetoe@users.noreply.github.com>
2024-07-14 19:46:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -39,9 +39,9 @@ window.onscroll = () => {
 /*==================== scroll reveal ====================*/
 ScrollReveal({
     // reset: true,
-    distance:'80px',
-    duration:2000,
-    delay:200
+    distance:'40px',
+    duration:1000,
+    delay:100
 });
 
 ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
